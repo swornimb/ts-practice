2f169ee5df4a600451ff4ee1e9fd91ecdd6da766
Swornim Bhattarai <swornim424@gmail.com>
2025-07-28 22:06:37 +0545

    finishing

--- ts-practice/src/component/HookForm.tsx
+++ ts-practice/src/component/HookForm.tsx
@@ -19,8 +19,35 @@ const onSubmit:SubmitHandler<FormData> = (data)=>{
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-            <input {...register('name',{required: "ABC"})} />
-            {errors.name&&<p>{errors.name.message}</p>}
+            <label>Name</label>
+            <input {...register('name',{required: "Name cannot be empty"})} />
+            {errors.name&&<p style={{color: "red"}}>{errors.name.message}</p>}
+        </div>
+        <div>
+            <label>Email</label>
+            <input {...register('email',{
+                required: "Email cannot be empty",
+                pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+                    message: "Email doesn't have a valid format"
+                }
+
+                })} 
+                
+                />
+            {errors.email&&<p style={{color: "red"}}>{errors.email.message}</p>}
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" {...register('password',{
+                required: "Password cannot be empty",
+                minLength: {
+                    value:8,
+                    message:"Password cannot be less that 8 letters"
+                }
+
+                })} />
+            {errors.password&&<p style={{color: "red"}}>{errors.password.message}</p>}
         </div>
         <button type="submit">Submit</button>
     </form>
